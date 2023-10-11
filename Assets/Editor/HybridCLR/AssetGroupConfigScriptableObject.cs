879bb001bf382bee3b06e20ed024f578822d5102
wangpeng <wangpeng19820714@outlook.com>
2023-10-11 19:54:59 +0800

    自動化打包相關内容

---
+++ Assets/Editor/HybridCLR/AssetGroupConfigScriptableObject.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct AssetGroupData
+{
+    public string GroupName;
+
+    public string FolderName;
+
+    public string Label;
+
+    public string Filter;
+
+    public string AssetType;
+
+    public string BuildPath;
+
+    public string LoadPath;
+}
+
+[CreateAssetMenu(menuName = "ScriptableObjects/AssetGroupSettingScriptableObject")]
+public class AssetGroupConfigScriptableObject : ScriptableObject
+{
+    public List<AssetGroupData> AssetGroupConfig;
+}
--- Assets/Editor/HybridCLR/BuildPackCommand.cs
+++ Assets/Editor/HybridCLR/BuildPackCommand.cs
@@ -4,13 +4,17 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEditor.AddressableAssets;
 using UnityEditor.AddressableAssets.Build;
 using UnityEditor.AddressableAssets.Settings;
-using UnityEditor.Graphs;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using System.Reflection;
+using UnityEditor.AddressableAssets.Build.Layout;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
+using UnityEditor.AddressableAssets.Settings.GroupSchemas;
 
 public class BuildPackCommand
 {
@@ -36,7 +40,35 @@ public class BuildPackCommand
         Debug.Log("BuildFinish path = " + m_Settings.RemoteCatalogBuildPath.GetValue(m_Settings));
     }
 
-    [MenuItem("HybridCLR/Build Pack/Print Path", priority = 3)]
+    [MenuItem("HybridCLR/Build Pack/Set Asset Group", priority = 3)]
+    public static void SetAddresablesAssets()
+    {
+        string settingPath = "Assets/Resources/Settings/Addressable/AssetGroupSettingData.asset";
+        AssetGroupConfigScriptableObject AssetsGroupSetting = AssetDatabase.LoadAssetAtPath<AssetGroupConfigScriptableObject>(settingPath);
+        AddressableAssetGroup group = null;
+        foreach (AssetGroupData data in AssetsGroupSetting.AssetGroupConfig)
+        {
+            string[] assets = GetAssets("Assets/" + data.FolderName, data.Filter);
+            switch (data.AssetType)
+            {
+                case "GameObject":
+                    group = CreateGroup(data.GroupName);
+                    break;
+                case "TextAsset":
+                    group = CreateGroup(data.GroupName);
+                    break;
+            }
+            foreach (var assetPath in assets)
+            {
+                string address = assetPath;
+                AddAssetEntry(group, assetPath, address);
+            }
+            EditorUtility.SetDirty(Settings);
+            AssetDatabase.Refresh();
+        }
+    }
+
+    [MenuItem("HybridCLR/Build Pack/Print Path", priority = 4)]
     public static void PrintBuildPath()
     {
         Debug.Log("BuildPath = " + Addressables.BuildPath);
@@ -89,18 +121,32 @@ public class BuildPackCommand
         return path;
     }
 
-    public static void SetAddresablesAssets()
-    {
-
-    }
-
      // 创建分组
-     static AddressableAssetGroup CreateGroup<T>(string groupName)
+     static AddressableAssetGroup CreateGroup(string groupName)
      {
          AddressableAssetGroup group = Settings.FindGroup(groupName);
          if (group == null)
-             group = Settings.CreateGroup(groupName, false, false, false, null, typeof(T));
-         Settings.AddLabel(groupName, false);
+        {
+            group = Settings.CreateGroup(groupName, false, false, false, null);
+        }
+        Settings.AddLabel(groupName, false);
+
+        ContentUpdateGroupSchema cugSchema = group.GetSchema<ContentUpdateGroupSchema>();
+        if (cugSchema == null)
+        {
+            cugSchema = group.AddSchema<ContentUpdateGroupSchema>();
+        }
+        cugSchema.StaticContent = false;
+
+        BundledAssetGroupSchema bagSchema = group.GetSchema<BundledAssetGroupSchema>();
+        var defultBAGSchema = Settings.DefaultGroup.GetSchema<BundledAssetGroupSchema>();
+        if (bagSchema == null)
+        {
+            bagSchema = group.AddSchema<BundledAssetGroupSchema>();
+        }
+        bagSchema.BuildPath.SetVariableByName(Settings, AddressableAssetSettings.kRemoteBuildPath);
+        bagSchema.LoadPath.SetVariableByName(Settings, AddressableAssetSettings.kRemoteLoadPath);
+
          return group;
      }
 
@@ -118,5 +164,74 @@ public class BuildPackCommand
         entry.address = address;
         entry.SetLabel(group.Name, true, false, false);
         return entry;
+    }
+
+    ///<summary>
+    /// 获取指定目录的资源
+    /// </summary>
+    /// <param name="filter">过滤器：
+    /// 若以t:开头，表示用unity的方式过滤;
+    /// 若以f:开头，表示用windows的SearchPattern方式过滤;
+    /// 若以r:开头，表示用正则表达式的方式过滤。</param>
+    public static string[] GetAssets(string folder, string filter)
+    {
+        if (string.IsNullOrEmpty(folder))
+            throw new ArgumentException("folder");
+        if (string.IsNullOrEmpty(filter))
+            throw new ArgumentException("filter");
+
+        folder = folder.TrimEnd('/').TrimEnd('\\');
+
+        if (filter.StartsWith("t:"))
+        {
+            string[] guids = AssetDatabase.FindAssets(filter, new string[] { folder });
+            string[] paths = new string[guids.Length];
+            for (int i = 0; i < guids.Length; i++)
+                paths[i] = AssetDatabase.GUIDToAssetPath(guids[i]);
+            return paths;
+        }
+        else if (filter.StartsWith("f:"))
+        {
+            string folderFullPath = Path.GetFullPath(folder);
+            string searchPattern = filter.Substring(2);
+            string[] files = Directory.GetFiles(folderFullPath, searchPattern, SearchOption.AllDirectories);
+            string[] paths = new string[files.Length];
+            for (int i = 0; i < files.Length; i++)
+                paths[i] = GetAssetPath(files[i]);
+            return paths;
+        }
+        else if (filter.StartsWith("r:"))
+        {
+            string folderFullPath = Path.GetFullPath(folder);
+            string pattern = filter.Substring(2);
+            string[] files = Directory.GetFiles(folderFullPath, "*.*", SearchOption.AllDirectories);
+            List<string> list = new List<string>();
+            for (int i = 0; i < files.Length; i++)
+            {
+                string name = Path.GetFileName(files[i]);
+                if (Regex.IsMatch(name, pattern) && !name.EndsWith(".meta"))
+                {
+                    string p = GetAssetPath(files[i]);
+                    list.Add(p);
+                }
+            }
+            return list.ToArray();
+        }
+        else
+        {
+            throw new InvalidOperationException("Unexpected filter: " + filter);
+        }
+    }
+
+    static AddressableAssetSettings Settings
+    {
+        get { return AddressableAssetSettingsDefaultObject.Settings; }
+    }
+
+    static string GetAssetPath(string path)
+    {
+        string root = Application.dataPath;
+        path = FormatFilePath(path);
+        return string.Format("Assets{0}", path.Replace(root, ""));
     }
 }
